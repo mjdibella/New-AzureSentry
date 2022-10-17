d329dccecf8c20a53fbc781b5e02ef97fdafa754
mjdibella <74016254+mjdibella@users.noreply.github.com>
2022-10-17 11:51:52 -0700

    Add files via upload

--- New-AzureSentry.ps1
+++ New-AzureSentry.ps1
@@ -5,9 +5,9 @@ param(
 	[Parameter(Mandatory=$true,Position=3)][string]$adminPassword,
 	[Parameter(Mandatory=$false,Position=4)][string]$storageContainerName = "copiedvhds",
 	[Parameter(Mandatory=$false,Position=5)][string]$vhdBlobName = "mobileiron-sentry.vhd",
-	[Parameter(Mandatory=$false,Position=6)][string]$vhdUri = "https://mobileironsentry.blob.core.windows.net/mobileironsentrycontainer/sentry-mobileiron-9.15.0-15.vhd",
-	[Parameter(Mandatory=$false,Position=7)][string]$deploymentTemplateUri = "https://mobileironsentry.blob.core.windows.net/mobileironsentrycontainer/SentryAzureDeploy-9.15.0-15.json",
-	[Parameter(Mandatory=$false,Position=8)][string]$deploymentParametersUri = "https://mobileironsentry.blob.core.windows.net/mobileironsentrycontainer/SentryAzureDeploy.parameters-9.15.0-15.json",
+	[Parameter(Mandatory=$false,Position=6)][string]$vhdUri = "https://mobileironsentry.blob.core.windows.net/mobileironsentrycontainer/sentry-mobileiron-9.16.0-9.vhd",
+	[Parameter(Mandatory=$false,Position=7)][string]$deploymentTemplateUri = "https://mobileironsentry.blob.core.windows.net/mobileironsentrycontainer/SentryAzureDeploy-9.16.0-9.json",
+	[Parameter(Mandatory=$false,Position=8)][string]$deploymentParametersUri = "https://mobileironsentry.blob.core.windows.net/mobileironsentrycontainer/SentryAzureDeploy.parameters-9.16.0-9.json",
 	[Parameter(Mandatory=$false,Position=9)][string]$subscription,
 	[Parameter(Mandatory=$false,Position=10)][string]$tags = ""
 )
